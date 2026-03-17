e111fcdc6561269df64b45abf374948fecfbcff1
Davenads <davidnadler9637@gmail.com>
2026-03-17 14:59:08 -0400

    Fix font CSS variables by applying them to html element via _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,29 +5,14 @@ import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { ThemeProvider } from 'next-themes';
-import { Inter, Montserrat } from 'next/font/google';
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap',
-});
-
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  variable: '--font-montserrat',
-  display: 'swap',
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <ErrorBoundary>
-        <div className={`${inter.variable} ${montserrat.variable}`}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
-        </div>
         <Analytics />
         <SpeedInsights />
       </ErrorBoundary>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import type { DocumentProps } from 'next/document';
+import { inter, montserrat } from '../lib/fonts';
 
 export default function Document(_props: DocumentProps) {
   return (
-    <Html lang="en">
+    <Html lang="en" className={`${inter.variable} ${montserrat.variable}`}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="DECUR - Data Exceeding Current Understanding of Reality" />
